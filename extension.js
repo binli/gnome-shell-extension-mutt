4f11411e293d547d35e4c39520ea336234bb7216
Bin Li <binli@opensuse.org>
2013-04-24 18:58:28 +0800

    init framework

---
+++ extension.js
@@ -0,0 +1,4 @@
+// Get current plugin, such as the path
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+// Import convenience in current plugin
+const Convenience = Me.imports.convenience;
