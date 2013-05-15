41162b7eb47688e8b79f6542a703e7ae7cc1acfe
Bin Li <binli@opensuse.org>
2013-05-15 17:57:14 +0800

    how to add icon in status area

--- extension.js
+++ extension.js
@@ -2,3 +2,39 @@
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 // Import convenience in current plugin
 const Convenience = Me.imports.convenience;
+
+const Gio = imports.gi.Gio;
+
+const Main = imports.ui.main;
+
+// The PanelMenu provide SystemStatusButton which could manage Sytem Status indicator
+const PanelMenu = imports.ui.panelMenu;
+
+let indicator;
+
+function MailIndicator() {
+    this._init.apply(this, arguments);
+}
+
+MailIndicator.prototype = {
+    __proto__: PanelMenu.SystemStatusButton.prototype,
+
+    _init: function() {
+        // init iconName, /usr/shar/icons/gnome/scalable/status
+        PanelMenu.SystemStatusButton.prototype._init.call(this, 'mail-read-symbolic');
+    }
+}
+
+function init() {
+}
+
+function enable() {
+    indicator = new MailIndicator();
+    // show indicator in status area
+    Main.panel.addToStatusArea('MailIndicator', indicator);
+}
+
+function disable() {
+    if (indicator !== null) indicator.destroy();
+    indicator = null;
+}
